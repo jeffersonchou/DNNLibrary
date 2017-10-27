557c647998473c9b273d68cc207db58e7f2b84e2
daquexian <daquexian566@gmail.com>
2017-10-27 09:35:15 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -26,39 +26,10 @@ Java_me_daquexian_nnapiexample_MainActivity_stringFromJNI(
 
     vector<uint32_t> dataDims{1, 28, 28, 1};
     vector<uint32_t> conv1BlobDims{1, 24, 24, 20};
-    vector<uint32_t> pool1BlobDims{1, 12, 12, 20};
-    vector<uint32_t> conv2BlobDims{1, 8, 8, 50};
-    vector<uint32_t> pool2BlobDims{1, 4, 4, 50};
-    vector<uint32_t> ip1BlobDims{1, 800};
-    vector<uint32_t> ip2BlobDims{1, 10};
-    vector<uint32_t> probBlobDims{1, 10};
-
-    vector<uint32_t> conv1WeightsDims{20, 5, 5, 1};
-    vector<uint32_t> conv2WeightsDims{50, 5, 5, 20};
-    vector<uint32_t> ip1WeightsDims{500, 800};
-    vector<uint32_t> ip2WeightsDims{800, 10};
 
     ANeuralNetworksOperandType dataType = getFloat32OperandTypeWithDims(dataDims);
 
     ANeuralNetworksOperandType conv1BlobType = getFloat32OperandTypeWithDims(conv1BlobDims);
-
-    ANeuralNetworksOperandType pool1BlobType = getFloat32OperandTypeWithDims(pool1BlobDims);
-
-    ANeuralNetworksOperandType conv2BlobType = getFloat32OperandTypeWithDims(conv2BlobDims);
-
-    ANeuralNetworksOperandType pool2BlobType = getFloat32OperandTypeWithDims(pool2BlobDims);
-
-    ANeuralNetworksOperandType ip1BlobType = getFloat32OperandTypeWithDims(ip1BlobDims);
-
-    ANeuralNetworksOperandType ip2BlobType = getFloat32OperandTypeWithDims(ip2BlobDims);
-
-    ANeuralNetworksOperandType conv1WeightsType = getFloat32OperandTypeWithDims(conv1WeightsDims);
-
-    ANeuralNetworksOperandType conv2WeightsType = getFloat32OperandTypeWithDims(conv2WeightsDims);
-
-    ANeuralNetworksOperandType ip1WeightsType = getFloat32OperandTypeWithDims(ip1WeightsDims);
-
-    ANeuralNetworksOperandType ip2WeightsType = getFloat32OperandTypeWithDims(ip2WeightsDims);
 
     ANeuralNetworksOperandType strideOneType;
     strideOneType.type = ANEURALNETWORKS_INT32;
@@ -70,16 +41,6 @@ Java_me_daquexian_nnapiexample_MainActivity_stringFromJNI(
     // Now we add the seven operands, in the same order defined in the diagram.
     ANeuralNetworksModel_addOperand(model, &dataType);  // operand 0
     ANeuralNetworksModel_addOperand(model, &conv1BlobType);  // operand 1
-    ANeuralNetworksModel_addOperand(model, &pool1BlobType); // operand 2
-    ANeuralNetworksModel_addOperand(model, &conv2BlobType);  // operand 3
-    ANeuralNetworksModel_addOperand(model, &pool2BlobType);  // operand 4
-    ANeuralNetworksModel_addOperand(model, &ip1BlobType); // operand 5
-    ANeuralNetworksModel_addOperand(model, &ip2BlobType);  // operand 6
-    ANeuralNetworksModel_addOperand(model, &conv1WeightsType);  // operand 7
-    ANeuralNetworksModel_addOperand(model, &conv2WeightsType);  // operand 8
-    ANeuralNetworksModel_addOperand(model, &ip1WeightsType);  // operand 9
-    ANeuralNetworksModel_addOperand(model, &ip2WeightsType);  // operand 10
-    ANeuralNetworksModel_addOperand(model, &strideOneType);  // operand 11
 
     return env->NewStringUTF(hello.c_str());
 
@@ -92,6 +53,8 @@ ANeuralNetworksOperandType getFloat32OperandTypeWithDims(std::vector<uint32_t> &
     type.zeroPoint = 0;  // These fields are useful for quantized tensors.
     type.dimensionCount = static_cast<uint32_t>(dims.size());
     type.dimensions = &dims[0];
+
+    return type;
 }
 
 jint throwException(JNIEnv *env, std::string message) {
