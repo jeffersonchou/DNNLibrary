2510cbb8e8d89ea80cf2185bd68ded5365582475
daquexian <daquexian566@gmail.com>
2017-11-16 20:00:49 +0800

    most simplified

--- dnnlibrary/src/main/cpp/JavaWrapper.cpp
+++ dnnlibrary/src/main/cpp/JavaWrapper.cpp
@@ -2,114 +2,148 @@
 // Created by daquexian on 2017/11/12.
 //
 
-#include <android/asset_manager_jni.h>
-#include "ModelBuilder.h"
-#include <map>
+#include <jni.h>
+#include <string>
+#include <vector>
+#include <android/NeuralNetworks.h>
 
-using std::string; using std::map;
-
-ModelBuilder builder;
-Model model;
-bool isUsing;
-
-jint throwException(JNIEnv *env, std::string message);
-
-
-extern "C"
-JNIEXPORT void
-JNICALL
-Java_me_daquexian_dnnlibrary_ModelWrapper_readFile(
-        JNIEnv *env,
-        jobject /* this */,
-        jobject javaAssetManager, jstring javaFilename) {
-
-    if (isUsing) {
-        throwException(env, "Please compile current model before generate a new model");
-    }
-
-    isUsing = true;
-
-    string filename = string(env->GetStringUTFChars(javaFilename, nullptr));
-    AAssetManager *mgrr = AAssetManager_fromJava(env, javaAssetManager);
-
-    builder.init(mgrr);
-
-    builder.readFromFile(filename);
-
-}
-
-extern "C"
-JNIEXPORT void
-JNICALL
-Java_me_daquexian_dnnlibrary_ModelWrapper_setOutput(
-        JNIEnv *env,
-        jobject /* this */,
-        jstring javaBlobName) {
-    if (!isUsing) {
-        throwException(env, "No model to add output");
-    }
-    string blobName = string(env->GetStringUTFChars(javaBlobName, nullptr));
-    builder.addIndexIntoOutput(builder.getBlobIndex(blobName));
-}
-
-extern "C"
-JNIEXPORT void
-JNICALL
-Java_me_daquexian_dnnlibrary_ModelWrapper_compile(
-        JNIEnv *env,
-        jobject /* this */,
-        jint preference) {
-    if (!isUsing) {
-        throwException(env, "No model to compile");
-    }
-
-    builder.compile(preference);
-
-    isUsing = false;
-}
+using std::string; using std::vector;
 
 extern "C"
 JNIEXPORT jfloatArray
 JNICALL
-Java_me_daquexian_dnnlibrary_ModelWrapper_predict(
+Java_me_daquexian_dnnlibrary_ModelWrapper_init(
         JNIEnv *env,
-        jobject /* this */,
-        jfloatArray dataArrayObject) {
+        jobject /* this */) {
 
-    builder.prepareForExecution(model);
+    ANeuralNetworksModel *model;
 
-    jfloat *data = env->GetFloatArrayElements(dataArrayObject, nullptr);
-    jsize dataLen = env->GetArrayLength(dataArrayObject);
+    ANeuralNetworksModel_create(&model);
+    int ret;
 
-    builder.setInputBuffer(model, builder.getInputIndexes()[0], data, static_cast<size_t>(dataLen));
+    vector<uint32_t> inputDim{1, 2, 3, 4};
+    ANeuralNetworksOperandType inputType;
+    inputType.type = ANEURALNETWORKS_TENSOR_FLOAT32;
+    inputType.scale = 0.f;
+    inputType.zeroPoint = 0;
+    inputType.dimensionCount = static_cast<uint32_t>(inputDim.size());
+    inputType.dimensions = &inputDim[0];
+    if ((ret = ANeuralNetworksModel_addOperand(model, &inputType)) != ANEURALNETWORKS_NO_ERROR) {   // operand 0
+        throw "Wrong return value";
+    }
+    if ((ret = ANeuralNetworksModel_addOperand(model, &inputType)) != ANEURALNETWORKS_NO_ERROR) {   // operand 1
+        throw "Wrong return value";
+    }
 
-    uint32_t outputLen = product(builder.getBlobDim(builder.getOutputIndexes()[0]));
-    float output[outputLen];
-    builder.setOutputBuffer(model, builder.getOutputIndexes()[0], output, sizeof(output));
+    ANeuralNetworksOperandType int32Type;
+    int32Type.type = ANEURALNETWORKS_INT32;
+    int32Type.scale = 0.f;
+    int32Type.zeroPoint = 0;
+    int32Type.dimensionCount = 0;
+    int32Type.dimensions = nullptr;
+    if ((ret = ANeuralNetworksModel_addOperand(model, &int32Type)) != ANEURALNETWORKS_NO_ERROR) {   // operand 2
+        throw "Wrong return value";
+    }
+    int32_t axis = 3;
+    if ((ret = ANeuralNetworksModel_setOperandValue(model, 2, &axis, sizeof(axis)) != ANEURALNETWORKS_NO_ERROR)) {
+        throw "Wrong return value";
+    }
 
-    model.predict();
+    vector<uint32_t> outputDim{1, 2, 3, 8};
+    ANeuralNetworksOperandType outputType;
+    outputType.type = ANEURALNETWORKS_TENSOR_FLOAT32;
+    outputType.scale = 0.f;
+    outputType.zeroPoint = 0;
+    outputType.dimensionCount = static_cast<uint32_t>(outputDim.size());
+    outputType.dimensions = &outputDim[0];
 
-    jfloatArray result = env->NewFloatArray(product(builder.getBlobDim(builder.getOutputIndexes()[0])));
-    env->SetFloatArrayRegion(result, 0, outputLen, output);
+    if ((ret = ANeuralNetworksModel_addOperand(model, &outputType)) != ANEURALNETWORKS_NO_ERROR) {   // operand 3
+        throw "Wrong return value";
+    }
+
+    uint32_t concatInputIndexes[3]{0, 1, 2};
+    uint32_t outputIndexes[1]{3};
+
+    ANeuralNetworksModel_addOperation(model, ANEURALNETWORKS_CONCATENATION,
+                                      3, concatInputIndexes, 1, outputIndexes);
+
+    uint32_t inputIndexes[2]{0, 1};
+    if ((ret = ANeuralNetworksModel_identifyInputsAndOutputs(
+            model,
+            2, inputIndexes,
+            1, outputIndexes)) != ANEURALNETWORKS_NO_ERROR) {
+
+        throw "Wrong return value";
+    }
+
+    ret = ANeuralNetworksModel_finish(model);
+    if (ret != ANEURALNETWORKS_NO_ERROR) {
+        throw "Wrong return value";
+    }
+
+    ANeuralNetworksCompilation *compilation = nullptr;
+
+    ret = ANeuralNetworksCompilation_create(model, &compilation);
+    if (ret != ANEURALNETWORKS_NO_ERROR) {
+        throw "Wrong return value";
+    }
+
+    ret = ANeuralNetworksCompilation_setPreference(compilation, ANEURALNETWORKS_PREFER_FAST_SINGLE_ANSWER);
+    if (ret != ANEURALNETWORKS_NO_ERROR) {
+        throw "Wrong return value";
+    }
+
+    ret = ANeuralNetworksCompilation_finish(compilation);
+    if (ret != ANEURALNETWORKS_NO_ERROR) {
+        throw "Wrong return value";
+    }
+
+    ANeuralNetworksExecution *execution = nullptr;
+    ANeuralNetworksExecution_create(compilation, &execution);
+
+    const size_t len = 1 * 2 * 3 * 4;
+
+    float data1[len];
+    float data2[len];
+
+    for (size_t i = 0; i < len; i++) {
+        data1[i] = 100000.5f;
+        data2[i] = -99999.5f;
+    }
+
+    ANeuralNetworksExecution_setInput(execution, 0, nullptr, data1, len);
+    ANeuralNetworksExecution_setInput(execution, 1, nullptr, data2, len);
+
+    float output[2 * len];
+    ANeuralNetworksExecution_setOutput(execution, 0, nullptr, output, 2 * len);
+
+    ANeuralNetworksEvent* event = nullptr;
+    if ((ret = ANeuralNetworksExecution_startCompute(execution, &event)) != ANEURALNETWORKS_NO_ERROR) {
+        throw "Wrong return value";
+    }
+
+    if ((ret = ANeuralNetworksEvent_wait(event)) != ANEURALNETWORKS_NO_ERROR) {
+        throw "Wrong return value";
+    }
+
+    ANeuralNetworksEvent_free(event);
+    ANeuralNetworksExecution_free(execution);
+
+    jfloatArray result = env->NewFloatArray(2 * len);
+    env->SetFloatArrayRegion(result, 0, 2 * len, output);
 
     return result;
 }
 
-extern "C"
-JNIEXPORT void
-JNICALL
-Java_me_daquexian_dnnlibrary_ModelWrapper_clear(
-        JNIEnv *env,
-        jobject /* this */) {
-    builder.clear();
-}
+__attribute__((weak))
+extern "C" int ANeuralNetworksModel_setInputsAndOutputs(
+        ANeuralNetworksModel *model, uint32_t inputCount, const uint32_t* inputs, uint32_t outputCount,
+        const uint32_t* outputs);
 
-jint throwException(JNIEnv *env, std::string message) {
-    jclass exClass;
-    std::string className = "java/lang/RuntimeException" ;
-
-    exClass = env->FindClass(className.c_str());
-
-    return env->ThrowNew(exClass, message.c_str());
+extern "C" int ANeuralNetworksModel_identifyInputsAndOutputs(
+        ANeuralNetworksModel *model, uint32_t inputCount, const uint32_t* inputs, uint32_t outputCount,
+        const uint32_t* outputs) {
+    return ANeuralNetworksModel_setInputsAndOutputs(model,
+            inputCount, inputs, outputCount, outputs);
 }
 
